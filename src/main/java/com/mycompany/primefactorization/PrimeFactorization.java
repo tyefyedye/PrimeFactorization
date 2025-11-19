41c918afa708b98be2aba4fce7925ae1a898c0ce
Cesar <Cesar@DESKTOP-LHJ76C7.mynetworksettings.com>
2025-11-19 08:34:48 -0500

    Add argument to limit number of factors found

--- src/main/java/com/mycompany/primefactorization/PrimeFactorization.java
+++ src/main/java/com/mycompany/primefactorization/PrimeFactorization.java
@@ -23,31 +23,35 @@ public class PrimeFactorization {
         BigInteger n = new BigInteger(args[0]);
         String method = args[1];
         long bound = Integer.parseInt(args[2]);
+        int limit = Integer.parseInt(args[3]);
         BigInteger a, f, f2;
-        
 
+        int count = 0;
         if(method.equals("pollard")) {
             numbersToFactor.add(n);
             PollardPMinusOne pollard = new PollardPMinusOne(bound);
-            while(!numbersToFactor.isEmpty()){
+            while((limit != 0 ? count < limit : true) && !numbersToFactor.isEmpty()){
                 a = numbersToFactor.removeFirst();
                 System.out.printf("Number to factor: %d\n", a);
                 f = pollard.getFactor(a);
                 f2 = a.divide(f);
                 System.out.printf("Factors found: [%d, %d]\n", f, f2);
                 checkIfPrimes(f, f2);
+                count = (limit != 0 ? count + 1 : 0);
             }
             printPrimeFactors(n);
         } else if (method.equals("quadratic")) {
             numbersToFactor.add(n);
             QuadraticSieve q = new QuadraticSieve();
-            while(!numbersToFactor.isEmpty()){
+            while((limit != 0 ? count < limit : true) && !numbersToFactor.isEmpty()){
                 a = numbersToFactor.removeFirst();
                 System.out.printf("Number to factor: %d\n", a);
                 f = q.getFactor(a, bound);
                 f2 = a.divide(f);
                 System.out.printf("Factors found: [%d, %d]\n", f, f2);
                 checkIfPrimes(f, f2);
+                count = (limit != 0 ? count + 1 : 0);
+                System.out.println(count);
             }
             printPrimeFactors(n);
         } 
